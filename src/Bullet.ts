9cbdeecb66cccdd3aae4c606eca9e1830baf302f
Victoria Ivanova <victoria.tihomirova@amusnet.com>
2026-04-15 17:33:31 +0300

    feat: add final implementation for the collision

--- src/Bullet.ts
+++ src/Bullet.ts
@@ -1,35 +1,43 @@
 import { Container, Graphics, Sprite } from "pixi.js";
 
 export class Bullet {
-    graphics:  Graphics | null = null;
+    shipBullet:  Graphics | null = null;
+    alienBullets: Graphics[] = [];
     constructor() {
-        this.graphics = null;
+        this.shipBullet = null;
     }
 
     public createBullet(ship: Sprite, world: Container) {
-        if(this.graphics){ // ако графиките не са нул и имаме графикс върни 
+        if(this.shipBullet){ // ако графиките не са нул и имаме графикс върни 
             return
         }
 
-        this.graphics = new Graphics(); // creating the bullet
+        this.shipBullet = new Graphics(); // creating the bullet
 
-        this.graphics.rect(-2, -10, 3, 10);
-        this.graphics.fill({ color: 0xff0000 });
+        this.shipBullet.rect(-2, -10, 3, 10);
+        this.shipBullet.fill({ color: 0xff0000 });
 
-        // this.graphics.position.set(ship.x, ship.y - 30);
-        this.graphics.position.set(270, 390)
-        console.log('bullet',this.graphics.position);
+        this.shipBullet.position.set(ship.x, ship.y - 30);
         
 
-        world.addChild(this.graphics); // add the bullet into the container(stage)
+        world.addChild(this.shipBullet); // add the bullet into the container(stage)
     }
+    public createEnemyBullet(world: Container, alien: Sprite){
+        const alienBullet = new Graphics();
 
+        alienBullet.rect(-2, -20, 3, 10);
+        alienBullet.fill({color: 16777215 }) // color white
+        alienBullet.position.set(alien.x, alien.y - 30);
+        this.alienBullets.push(alienBullet);
+
+        world.addChild(alienBullet)
+    }
     public moveBullet(world: Container) {
-        if (this.graphics) {
-            this.graphics.y -= 8;
-            if (this.graphics.y < 0) {
-                world.removeChild(this.graphics);
-                this.graphics = null;
+        if (this.shipBullet) {
+            this.shipBullet.y -= 8;
+            if (this.shipBullet.y < 0) {
+                world.removeChild(this.shipBullet);
+                this.shipBullet = null;
             }
         }
     }
